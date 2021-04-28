757e9c470efd110d0007dd05fa8eee77b38d0859
Oyesanmi Fiyinfoluwa <fiyin001@gmail.com>
2021-04-28 12:39:47 +0100

    update

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.forms import UserCreationForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Post
+from django.views import generic
 
 # Create your views here.
+class BlogRegisterView(generic.CreateView):
+    form_class = UserCreationForm
+    #model = Post
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
 class BlogListView(ListView):
     model = Post
     template_name = 'home.html'
@@ -15,3 +24,13 @@ class BlogCreateView(CreateView):
     model = Post
     template_name = 'post_new.html'
     fields = ['title', 'author', 'body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
